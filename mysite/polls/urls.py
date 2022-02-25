df36633823f8d01d5f40ed6d7a679932b03b3eef
ambri <bachukilabadze@gmail.com>
2022-02-25 04:32:22 +0400

    second commit

---
+++ mysite/polls/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+
+
+from . import views
+
+# which app view to create for a url when using the {% url %} template tag
+app_name = 'polls'
+
+urlpatterns = [
+    # ex: /polls/
+    path('', views.IndexView.as_view(), name='index'),
+    # ex: /polls/5/
+    # added the word 'specifics'
+    path('specifics/<int:pk>/', views.DetailView.as_view(), name='detail'),
+    # ex: /polls/5/results/
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
